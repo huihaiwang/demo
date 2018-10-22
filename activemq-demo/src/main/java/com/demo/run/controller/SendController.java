20925d80d3e7afe43d4cc4addcf5faa6252f5033
whh <619642725@qq.com>
2018-10-22 23:50:08 +0800

    mysql定时器

---
+++ activemq-demo/src/main/java/com/demo/run/controller/SendController.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: SendController
+ * Author:   94hai
+ * Date:     2018/10/18 20:23
+ * Description: 发送者
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.demo.run.controller;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈发送者〉
+ *
+ * @author 94hai
+ * @create 2018/10/18
+ * @since 1.0.0
+ */
+public class SendController {
+
+}
---
+++ activemq-demo/src/main/java/com/demo/run/mq/ConsumerMessageListener.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: ConsumerMessageListener
+ * Author:   94hai
+ * Date:     2018/10/18 19:34
+ * Description: a
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.demo.run.mq;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈a〉
+ *
+ * @author 94hai
+ * @create 2018/10/18
+ * @since 1.0.0
+ */
+public class ConsumerMessageListener {
+
+}
